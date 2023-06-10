a9d0976b0ede05116e738173b8f07809db27bda7
TaxiKab119 <cs.k.alexanderbalan@gmail.com>
2023-06-10 14:31:42 -0400

    Finish category list ui

--- app/src/main/java/com/example/mycityapp/ui/MyCityCategoriesScreen.kt
+++ app/src/main/java/com/example/mycityapp/ui/MyCityCategoriesScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
@@ -22,6 +24,23 @@ import com.example.compose.MyCityAppTheme
 import com.example.mycityapp.R
 import com.example.mycityapp.data.DataSource
 import com.example.mycityapp.model.Category
+
+@Composable
+fun CategoryListContent(
+    categoryList: List<Category>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier) {
+        items(categoryList) {category ->
+            CategoryCard(
+                category = category,
+                selected = false,
+                onCardClick = { /*TODO*/ },
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -73,5 +92,13 @@ fun CategoryCardPreview() {
             selected = false,
             onCardClick = {}
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryListContentPreview() {
+    MyCityAppTheme {
+        CategoryListContent(categoryList = DataSource.getCategoryData())
     }
 }
