a1c531238a04fa7132ceb7a21d025b6132b68ebd
TaxiKab119 <cs.k.alexanderbalan@gmail.com>
2023-06-10 12:12:20 -0400

    Create category card ui

--- app/src/main/java/com/example/mycityapp/ui/MyCityCategoriesScreen.kt
+++ app/src/main/java/com/example/mycityapp/ui/MyCityCategoriesScreen.kt
@@ -1,2 +1,77 @@
 package com.example.mycityapp.ui
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose.MyCityAppTheme
+import com.example.mycityapp.R
+import com.example.mycityapp.data.DataSource
+import com.example.mycityapp.model.Category
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CategoryCard(
+    category: Category,
+    selected: Boolean,
+    onCardClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        colors = CardDefaults.cardColors(
+            containerColor = if (selected)
+                MaterialTheme.colorScheme.primaryContainer
+            else
+                MaterialTheme.colorScheme.secondaryContainer
+        ),
+        onClick = onCardClick
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = category.icon),
+                modifier = Modifier
+                    .size(48.dp),
+                contentDescription = stringResource(id = category.titleResourceId)
+            )
+            Text(
+                text = stringResource(id = category.titleResourceId),
+                style = MaterialTheme.typography.displayMedium,
+                modifier = Modifier.padding(start = 12.dp)
+            )
+
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryCardPreview() {
+    MyCityAppTheme {
+        CategoryCard(
+            category = DataSource.defaultCategory,
+            selected = false,
+            onCardClick = {}
+        )
+    }
+}
